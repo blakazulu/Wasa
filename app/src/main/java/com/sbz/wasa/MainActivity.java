2b8de09e16cb7c69881d38893faf2bf5df7c0e2a
Shaka Blaka Zulu <liraz1234@hotmail.com>
2020-10-13 00:56:53 +0300

    added launch screen

--- app/src/main/java/com/sbz/wasa/MainActivity.java
+++ app/src/main/java/com/sbz/wasa/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
         // hide action bar
